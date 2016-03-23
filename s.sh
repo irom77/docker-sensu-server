4a58f352ecc7a2b26800e66f11298ef571c3e8fc
Irek Romaniuk <irek.romaniuk@gmail.com>
2016-03-22 20:03:16 -0400

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t sensu-server .
-docker run -d --name sensu-server -h sensu-server -p 10022:22 -p 3000:3000 -p 4567:4567 -p 5671:5671 -p 15672:15672 sensu-server
+docker run -d --name sensu-server -h sensu-server -p 10022:22 -p 3005:3005 -p 4567:4567 -p 5671:5671 -p 15672:15672 sensu-server
