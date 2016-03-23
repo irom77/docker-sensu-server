953854fbaa84f3cd1c0ef7494a0627ebf3bfe0a2
Irek Romaniuk <irek.romaniuk@gmail.com>
2016-03-22 20:27:31 -0400

    Delete s.sh

--- bin/s.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t sensu-ent-server .
-docker run -d --name sensu-ent-server -h sensu-ent-server -p 10022:22 -p 3005:3005 -p 4567:4567 -p 5671:5671 -p 15672:15672 sensu-ent-server
